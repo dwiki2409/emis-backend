dbb459f7edb378c85a5644f8807049ed149ecbc3
1217050089 <1217050089@student.uinsgd.ac.id>
2023-12-26 17:31:04 +0700

    backend upload

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import {
+  fileURLToPath
+} from 'url'
+import {
+  dirname
+} from 'path'
 import path from 'path'
 import connectDB from './db/connectDB.js'
 import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes.js'
-
+import cors from 'cors'
 import subjectRoutes from './routes/subjectRoutes.js'
 import taskRoutes from './routes/taskRoutes.js'
 import notesRotes from './routes/notesRoutes.js'
@@ -16,11 +20,16 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.use(
+  cors()
+)
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.json()) // for parsing application/json data in the request body
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded data in the request body
+app.use(express.urlencoded({
+  extended: true
+})) // for parsing application/x-www-form-urlencoded data in the request body
 app.use(cookieParser())
 
 // Routes
@@ -29,7 +38,8 @@ app.use('/v1/api/subjects', subjectRoutes)
 app.use('/v1/api/tasks', taskRoutes)
 app.use('/v1/api/notes', notesRotes)
 
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(
+  import.meta.url)
 const __dirname = dirname(__filename)
 
 const rootPath = path.resolve(__dirname, '../')
